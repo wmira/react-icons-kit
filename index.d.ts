02eb3e3dbef8cb24bd00f6c6eb7396b73a1e807e
Warren <warrenmira@gmail.com>
2018-09-16 20:27:03 +0800

    Fix issues with ts declaration

--- index.d.ts
+++ index.d.ts
@@ -2,41 +2,39 @@
 
 declare module "react-icons-kit" {
     import * as React from 'react'
-
-
-    interface IconProps {
+    
+    export interface IconProp {
+        icon: any
         size?: number | string
-        style?: object
+        style?: React.StyleHTMLAttributes<any>
         className?: string
-        icon: object
     }
-    const Icon: React.SFC<any>
     const withBaseIcon: (props: IconProps) => React.SFC<any>
+    export class Icon extends React.Component<IconProp> {}
 }
 
-declare module "react-icons-kit/icomoon" {
-}
-declare module "react-icons-kit/md" {
-}
-declare module "react-icons-kit/fa" {
-}
-declare module "react-icons-kit/iconic" {
-}
-declare module "react-icons-kit/entypo" {
-}
-declare module "react-icons-kit/metrize" {
-}
-declare module "react-icons-kit/ikons" {
-}
-declare module "react-icons-kit/linea" {
-}
-declare module "react-icons-kit/ionicons" {
-}
-declare module "react-icons-kit/oct" {
-}
-declare module "react-icons-kit/typicons" {
-}
-declare module "react-icons-kit/noto_emoji_regular" {
-}
-declare module "react-icons-kit/feather" {
-}
+declare module "react-icons-kit/icomoon"
+declare module "react-icons-kit/md"
+
+declare module "react-icons-kit/fa"
+
+declare module "react-icons-kit/iconic"
+
+declare module "react-icons-kit/entypo"
+
+declare module "react-icons-kit/metrize"
+
+declare module "react-icons-kit/ikons"
+
+declare module "react-icons-kit/linea"
+
+declare module "react-icons-kit/ionicons"
+
+declare module "react-icons-kit/oct"
+
+declare module "react-icons-kit/typicons"
+
+declare module "react-icons-kit/noto_emoji_regular"
+
+declare module "react-icons-kit/feather"
+    
