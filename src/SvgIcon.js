dd9e95a8f959f30419c73d828adfa00e2d0e4dc6
Warren Mira <warrenmira@gmail.com>
2021-07-29 20:23:54 +0800

    need to correctly expand style

--- src/SvgIcon.js
+++ src/SvgIcon.js
@@ -3,6 +3,19 @@ import React, { createElement } from 'react';
 import PropTypes from 'prop-types';
 import camelcase from 'camel-case';
 
+function notNullOrUndef(val) {
+  return (val !== null && val !== undefined)
+}
+function expandStyle(style = '') {
+  return style.split(';').reduce((partial, next) => {
+    // next h ere is key:val
+    const [key, val] = next.split(':');
+    if (notNullOrUndef(key) && notNullOrUndef(val)) {
+      partial[camelcase(key)] = val;
+    }
+    return partial;
+  }, {})
+}
 const walkChildren = (children) => {
     return children.map((child, idx ) => {
         const { name, attribs: attribsMap, children: gchildren = null } = child;
@@ -11,15 +24,19 @@ const walkChildren = (children) => {
         const attribs = Object.keys(attribsMap)
             .filter( key => key !== 'fill' && key !== 'stroke' && attribsMap[key] !== 'none' )
             .reduce( (partial, key) => {
-
-                partial[camelcase(key)] = attribsMap[key];
+                if (key === 'style') {
+                  partial.style = expandStyle(attribsMap[key]);
+                } else {
+                  partial[camelcase(key)] = attribsMap[key];
+                }
                 return partial;
             }, {} );
         //special case, it has fill and stroke at the same time
         let merge = {};
         if ( attribsMap.fill === 'none' && attribsMap.stroke ) {
             merge = { fill: 'none', stroke: 'currentColor' };
-        }
+        }        
+        
         return createElement(name, { key: idx, ...attribs, ...merge }, gchildren === null ? gchildren : walkChildren(gchildren));
     });
 };
